135eff1867ef72f5a8ea41a97397a43184dbced3
AlexandroHenry <alaxhenry0121@gmail.com>
2024-02-08 09:19:30 +0900

    젠킨스 파일 업로드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,7 @@ android {
         multiDexEnabled = true
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
